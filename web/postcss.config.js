4207048e1d49459f16ed84175fcce25337cba8b7
Taylor Covington <taylorcovington@Taylors-Air.home>
2021-11-07 06:11:19 -0700

    added tailwind, initial hero created

--- web/postcss.config.js
+++ web/postcss.config.js
@@ -1,5 +1,6 @@
 module.exports = () => ({
   plugins: [
+    require("tailwindcss"),
     require("postcss-preset-env")({
       stage: 3,
       features: {
--- web/src/pages/Landing.js
+++ web/src/pages/Landing.js
@@ -1,7 +1,8 @@
 import React from "react";
+import Hero from "../components/Hero";
 
 const LandingPage = () => {
-  return <p>Landing page</p>;
+  return <Hero />;
 };
 
 export default LandingPage;
