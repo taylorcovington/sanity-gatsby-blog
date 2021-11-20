e408e4e1eb98580a9f4c34aa08113f0b7cb4620b
Taylor Covington <taylorcovington@Taylors-Air.home>
2021-11-19 21:15:05 -0700

    adding nav links

--- web/src/components/Hero.js
+++ web/src/components/Hero.js
@@ -99,7 +99,9 @@ export default function Hero({ pageData }) {
                     className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3"
                   >
                     <a
-                      href="#"
+                      href={
+                        cta.type === "primary" ? "#contactMe" : "#learnMore"
+                      }
                       className={
                         cta.type === "primary"
                           ? "w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
