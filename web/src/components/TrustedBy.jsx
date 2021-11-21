4c282e270739e1fa43255133d589820a6d721bf0
Taylor Covington <taylorcovington@Taylors-Air.home>
2021-11-21 06:17:30 -0700

    adding trusted by section

---
+++ web/src/components/TrustedBy.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import fidelityLogo from "../assets/fidelity-logo.png";
+import symplyLogo from "../assets/logo.png";
+import linkHcmLogo from "../assets/linkhcm-logo.png";
+
+const TrustedBy = () => {
+  return (
+    <>
+      <div className="bg-indigo-200 bg-opacity-25">
+        <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+          <div className="lg:grid lg:grid-cols-2 lg:gap-8">
+            <h2 className="max-w-md mx-auto text-3xl font-extrabold text-indigo-900 text-center lg:max-w-xl lg:text-left">
+              I've had the opportunity to work with these great companies
+            </h2>
+            <div className="flow-root self-center mt-8 lg:mt-0">
+              <div className="-mt-4 -ml-8 flex flex-wrap justify-between lg:-ml-4">
+                <div className="mt-4 ml-8 flex flex-grow flex-shrink-0 justify-center lg:flex-grow-0 lg:ml-4">
+                  <img
+                    className="h-12"
+                    src={fidelityLogo}
+                    alt="Fidelity logo"
+                  />
+                </div>
+                <div className="mt-4 ml-8 flex flex-grow flex-shrink-0 justify-center lg:flex-grow-0 lg:ml-4">
+                  <img className="h-12" src={symplyLogo} alt="Symply logo" />
+                </div>
+                <div className="mt-4 ml-8 flex flex-grow flex-shrink-0 justify-center lg:flex-grow-0 lg:ml-4">
+                  <img className="h-12" src={linkHcmLogo} alt="Level" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TrustedBy;
