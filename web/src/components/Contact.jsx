88013bf2c1b9c84bf1e3d2dc1156ffb666a7f0ab
Taylor Covington <taylorcovington@Taylors-Air.home>
2021-11-21 07:58:02 -0700

    updating contact form to work with netlify forms

--- web/src/components/Contact.jsx
+++ web/src/components/Contact.jsx
@@ -51,10 +51,12 @@ const ContactMe = () => {
         <div className="bg-white py-16 px-4 sm:px-6 lg:col-span-3 lg:py-24 lg:px-8 xl:pl-12">
           <div className="max-w-lg mx-auto lg:max-w-none">
             <form
-              action="#"
+              action="/"
+              name="contact-form"
               method="POST"
+              data-netlify="true"
+              netlify-honeypot="bot-field"
               className="grid grid-cols-1 gap-y-6"
-              netlify
             >
               <div>
                 <label htmlFor="full-name" className="sr-only">
